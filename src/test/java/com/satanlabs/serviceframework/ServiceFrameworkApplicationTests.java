9b1d947c1d24d81a19ba0ed2213a649fcf5b9aa3
SatanLabs, Inc <46850618+satanlabs@users.noreply.github.com>
2022-08-31 23:32:00 +0530

    zeroth commit
    added code

---
+++ src/test/java/com/satanlabs/serviceframework/ServiceFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satanlabs.serviceframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFrameworkApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
