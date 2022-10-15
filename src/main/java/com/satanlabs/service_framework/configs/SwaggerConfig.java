472ced613ddae57fb53cd09f5c90648c7c9f8b42
SatanLabs, Inc <46850618+satanlabs@users.noreply.github.com>
2022-10-15 19:38:07 +0530

    version bump up
     - removing springfox, NOT working with Spring

--- src/main/java/com/satanlabs/service_framework/configs/SwaggerConfig.java
+++ src/main/java/com/satanlabs/service_framework/configs/SwaggerConfig.java
@@ -1,126 +1,131 @@
 package com.satanlabs.service_framework.configs;
-
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.MediaType;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
-
-import java.lang.reflect.Field;
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static springfox.documentation.builders.PathSelectors.regex;
+//
+//import org.springframework.beans.BeansException;
+//import org.springframework.beans.factory.config.BeanPostProcessor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.http.MediaType;
+//import org.springframework.util.ReflectionUtils;
+//import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.*;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spi.service.contexts.SecurityContext;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+//import springfox.documentation.swagger.web.SecurityConfiguration;
+//import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+//
+//import java.lang.reflect.Field;
+//import java.util.*;
+//import java.util.stream.Collectors;
+//
+//import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * Created By Abhinav Tripathi on 2019-09-13
  */
-@Profile({"!prod"})
-@Configuration
+//@Profile({"!prod"})
+//@Configuration
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo()).pathMapping("/")
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Arrays.asList(new ApiKey("Authorization", "Authorization", "header")))
-                .securityContexts(securityContexts())
-                .produces(Collections.singleton(MediaType.APPLICATION_JSON_VALUE))
-                .consumes(Collections.singleton(MediaType.APPLICATION_JSON_VALUE))
-                .enableUrlTemplating(true);
-    }
 
-    private List<ApiKey> apiKeys() {
-        List<ApiKey> apiKeys = new ArrayList<>();
-        apiKeys.add(new ApiKey("Authorization", "Authorization", "header"));
-        return apiKeys;
-    }
-
-    private List<SecurityContext> securityContexts() {
-        List<SecurityContext> securityContexts = new ArrayList<>();
-        SecurityContext securityContext = SecurityContext
-                .builder()
-                .securityReferences(defaultAuth())
-                .forPaths(regex("/.*"))
-                .build();
-        securityContexts.add(securityContext);
-        return securityContexts;
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        List<SecurityReference> securityReferences = new ArrayList<>();
-        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));  //Here we use the same key defined in the security scheme oms_key
-        // securityReferences.add(new SecurityReference("client_id", authorizationScopes));
-        //securityReferences.add(new SecurityReference("requested_by", authorizationScopes));
-        return securityReferences;
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("APIs Documentation")
-                .version("1.0")
-                .build();
-    }
-
-    @Bean
-    public SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder()
-                .clientId("X-Client-Id")
-                .clientSecret("Authorization")
-                .scopeSeparator(" ")
-                .useBasicAuthenticationWithAccessCodeGrant(true)
-                .build();
-    }
-
-    @Bean
-    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
-        return new BeanPostProcessor() {
-
-            @Override
-            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-                if (bean instanceof WebMvcRequestHandlerProvider) {
-                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
-                }
-                return bean;
-            }
-
-            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
-                List<T> copy = mappings.stream()
-                        .filter(mapping -> mapping.getPatternParser() == null)
-                        .collect(Collectors.toList());
-                mappings.clear();
-                mappings.addAll(copy);
-            }
-
-            @SuppressWarnings("unchecked")
-            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
-                try {
-                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
-                    field.setAccessible(true);
-                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
-                } catch (IllegalArgumentException | IllegalAccessException e) {
-                    throw new IllegalStateException(e);
-                }
-            }
-        };
-    }
+    /**
+     * THIS class has been commented because SpringFox's bean was giving NPE.
+     * Decided to NOT use this after all.
+     */
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .apiInfo(apiInfo()).pathMapping("/")
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build()
+//                .securitySchemes(Arrays.asList(new ApiKey("Authorization", "Authorization", "header")))
+//                .securityContexts(securityContexts())
+//                .produces(Collections.singleton(MediaType.APPLICATION_JSON_VALUE))
+//                .consumes(Collections.singleton(MediaType.APPLICATION_JSON_VALUE))
+//                .enableUrlTemplating(true);
+//    }
+//
+//    private List<ApiKey> apiKeys() {
+//        List<ApiKey> apiKeys = new ArrayList<>();
+//        apiKeys.add(new ApiKey("Authorization", "Authorization", "header"));
+//        return apiKeys;
+//    }
+//
+//    private List<SecurityContext> securityContexts() {
+//        List<SecurityContext> securityContexts = new ArrayList<>();
+//        SecurityContext securityContext = SecurityContext
+//                .builder()
+//                .securityReferences(defaultAuth())
+//                .forPaths(regex("/.*"))
+//                .build();
+//        securityContexts.add(securityContext);
+//        return securityContexts;
+//    }
+//
+//    private List<SecurityReference> defaultAuth() {
+//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//        authorizationScopes[0] = authorizationScope;
+//        List<SecurityReference> securityReferences = new ArrayList<>();
+//        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));  //Here we use the same key defined in the security scheme oms_key
+//        // securityReferences.add(new SecurityReference("client_id", authorizationScopes));
+//        //securityReferences.add(new SecurityReference("requested_by", authorizationScopes));
+//        return securityReferences;
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("APIs Documentation")
+//                .version("1.0")
+//                .build();
+//    }
+//
+//    @Bean
+//    public SecurityConfiguration security() {
+//        return SecurityConfigurationBuilder.builder()
+//                .clientId("X-Client-Id")
+//                .clientSecret("Authorization")
+//                .scopeSeparator(" ")
+//                .useBasicAuthenticationWithAccessCodeGrant(true)
+//                .build();
+//    }
+//
+//    @Bean
+//    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+//        return new BeanPostProcessor() {
+//
+//            @Override
+//            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+//                if (bean instanceof WebMvcRequestHandlerProvider) {
+//                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+//                }
+//                return bean;
+//            }
+//
+//            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+//                List<T> copy = mappings.stream()
+//                        .filter(mapping -> mapping.getPatternParser() == null)
+//                        .collect(Collectors.toList());
+//                mappings.clear();
+//                mappings.addAll(copy);
+//            }
+//
+//            @SuppressWarnings("unchecked")
+//            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+//                try {
+//                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+//                    field.setAccessible(true);
+//                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+//                } catch (IllegalArgumentException | IllegalAccessException e) {
+//                    throw new IllegalStateException(e);
+//                }
+//            }
+//        };
+//    }
 }
